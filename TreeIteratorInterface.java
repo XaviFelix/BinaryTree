0d27fd5932c3837a0b242d6e8efe88cdb9131fdc
xfelix <xavierfelix74@gmail.com>
2024-04-02 15:25:24 -0700

    Possible function

--- TreeIteratorInterface.java
+++ TreeIteratorInterface.java
@@ -8,5 +8,8 @@ public interface TreeIteratorInterface<T>
     public Iterator<T> getPostorderIterator();
     public Iterator<T> getInorderIterator();
     public Iterator<T> getLevelOrderIterator();
+
+    //Possible function:
+    public Iterator<T> getLevelOrderIteratorVersion2();
         
 }//End TreeIteratorInterface
